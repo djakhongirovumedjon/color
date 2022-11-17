d7b355fc348ded5ed12de1d53ea0956c584bed1e
djakhongirovumedjon <117729551+djakhongirovumedjon@users.noreply.github.com>
2022-11-17 20:32:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,187 @@
+let canvas = document.getElementById("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Initialize the GL context
+let gl = canvas.getContext('webgl');
+if(!gl){
+  console.error("Unable to initialize WebGL.");
+}
+
+//Time
+let time = 0.0;
+
+//************** Shader sources **************
+
+let vertexSource = `
+attribute vec2 position;
+void main() {
+  gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+
+let fragmentSource = `
+precision highp float;
+
+#define AA
+
+uniform float width;
+uniform float height;
+vec2 resolution = vec2(width, height);
+
+uniform float time;
+
+void main(){
+
+	float strength = 0.4;
+	float t = time/6.0;
+
+	vec3 col = vec3(0);
+	vec2 fC = gl_FragCoord.xy;
+
+	#ifdef AA
+	for(int i = -1; i <= 1; i++) {
+		for(int j = -1; j <= 1; j++) {
+
+			fC = gl_FragCoord.xy+vec2(i,j)/3.0;
+
+			#endif
+
+			//Normalized pixel coordinates (from 0 to 1)
+			vec2 pos = fC/resolution.xy;
+
+			pos.y /= resolution.x/resolution.y;
+			pos = 4.0*(vec2(0.5) - pos);
+
+			for(float k = 1.0; k < 7.0; k+=1.0){ 
+				pos.x += strength * sin(2.0*t+k*1.5 * pos.y)+t*0.5;
+				pos.y += strength * cos(2.0*t+k*1.5 * pos.x);
+			}
+
+			//Time letying pixel colour
+			col += 0.5 + 0.5*cos(time+pos.xyx+vec3(0,2,4));
+
+			#ifdef AA
+		}
+	}
+
+	col /= 9.0;
+	#endif
+
+  //Gamma
+  col = pow(col, vec3(0.4545));
+
+	//Fragment colour
+	gl_FragColor = vec4(col,1.0);
+}
+`;
+
+//************** Utility functions **************
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+	gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.uniform1f(widthHandle, window.innerWidth);
+  gl.uniform1f(heightHandle, window.innerHeight);
+}
+
+
+//Compile shader and combine with source
+function compileShader(shaderSource, shaderType){
+  let shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+  	throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+  }
+  return shader;
+}
+
+//From https://codepen.io/jlfwong/pen/GqmroZ
+//Utility to complain loudly if we fail to find the attribute/uniform
+function getAttribLocation(program, name) {
+  let attributeLocation = gl.getAttribLocation(program, name);
+  if (attributeLocation === -1) {
+  	throw 'Cannot find attribute ' + name + '.';
+  }
+  return attributeLocation;
+}
+
+function getUniformLocation(program, name) {
+  let attributeLocation = gl.getUniformLocation(program, name);
+  if (attributeLocation === -1) {
+  	throw 'Cannot find uniform ' + name + '.';
+  }
+  return attributeLocation;
+}
+
+//************** Create shaders **************
+
+//Create vertex and fragment shaders
+let vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
+let fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
+
+//Create shader programs
+let program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+//Set up rectangle covering entire canvas 
+let vertexData = new Float32Array([
+  -1.0,  1.0, 	// top left
+  -1.0, -1.0, 	// bottom left
+   1.0,  1.0, 	// top right
+   1.0, -1.0, 	// bottom right
+]);
+
+
+let vertexDataBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+
+let positionHandle = getAttribLocation(program, 'position');
+
+gl.enableVertexAttribArray(positionHandle);
+gl.vertexAttribPointer(positionHandle,
+  2, 				
+  gl.FLOAT, 
+  false, 		
+  2 * 4, 		
+  0 				
+  );
+
+
+let timeHandle = getUniformLocation(program, 'time');
+let widthHandle = getUniformLocation(program, 'width');
+let heightHandle = getUniformLocation(program, 'height');
+
+gl.uniform1f(widthHandle, window.innerWidth);
+gl.uniform1f(heightHandle, window.innerHeight);
+
+let lastFrame = Date.now();
+let thisFrame;
+
+function draw(){
+	
+
+	thisFrame = Date.now();
+  time += (thisFrame - lastFrame)/770;	
+	lastFrame = thisFrame;
+
+	
+  gl.uniform1f(timeHandle, time);
+  
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  requestAnimationFrame(draw);
+}
+
+draw();
